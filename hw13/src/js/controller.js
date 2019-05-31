b1544e67d0845be7d57dc60fc0ad192ed0b3737a
OlegGap <hnapovskiy20@gmail.com>
2019-05-31 17:52:09 +0300

    add hw13

---
+++ hw13/src/js/controller.js
@@ -0,0 +1,22 @@
+export default class Controller {
+  constructor(model, view) {
+    this.model = model;
+    this.view = view;
+
+    view.on("add", this.addCard.bind(this));
+    view.on("remove", this.removeCard.bind(this));
+    this.model.updateLocStor();
+    this.view.updateViwe(this.model.cards); //відрисує закладки при завантажені
+  }
+
+  addCard(input) {
+    if (this.model.isNotCorect(input)) {
+      this.view.addCard(this.model.isNotCorect(input));
+    } else {
+      this.model.addCard(input).then(cards => this.view.addCard(cards));
+    }
+  }
+  removeCard(curentURL) {
+    this.view.updateViwe(this.model.removeCard(curentURL));
+  }
+}
